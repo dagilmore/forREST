dfabc7f1d5a0747555f3bbfd478fe02a8238cc7e
David Gilmore <dagilmore91@gmail.com>
2013-11-30 16:26:11 -0600

    Fix failing ingestion test

---
+++ src/main/java/com/forrest/web/config/StandaloneConfig.java
@@ -0,0 +1,13 @@
+package com.forrest.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author David Gilmore
+ * @date 11/30/13
+ */
+@Configuration
+@ComponentScan("com.forrest")
+public class StandaloneConfig {
+}
--- src/test/java/com/forrest/core/services/ingestion/InjestJobTest.java
+++ src/test/java/com/forrest/core/services/ingestion/InjestJobTest.java
@@ -1,10 +1,12 @@
 package com.forrest.core.services.ingestion;
 
 import au.com.bytecode.opencsv.CSVReader;
+import com.forrest.web.config.StandaloneConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
 import static org.easymock.EasyMock.*;
 
@@ -13,6 +15,7 @@ import static org.easymock.EasyMock.*;
  * @date 11/30/13
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=StandaloneConfig.class, loader=AnnotationConfigContextLoader.class)
 public class InjestJobTest {
 
     @Test
