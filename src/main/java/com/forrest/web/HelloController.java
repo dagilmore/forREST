bb82e72c7f9457725e0eeb823eaf8948392616aa
David Gilmore <dagilmore91@gmail.com>
2013-11-30 13:10:52 -0600

    Hello all

--- src/main/java/com/forrest/web/HelloController.java
+++ src/main/java/com/forrest/web/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
+		model.addAttribute("message", "Hello All!");
 		return "hello";
 	}
 }
