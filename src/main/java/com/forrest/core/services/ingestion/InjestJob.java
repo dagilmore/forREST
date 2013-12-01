486921e812de2af97ea0e46452cc470ddf9bafad
iamtrask <liamtrask@gmail.com>
2013-11-30 20:29:48 -0600

    added basic hive method

--- src/main/java/com/forrest/core/services/ingestion/InjestJob.java
+++ src/main/java/com/forrest/core/services/ingestion/InjestJob.java
@@ -1,5 +1,14 @@
 package com.forrest.core.services.ingestion;
 
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author David Gilmore
  * @date 11/30/13
@@ -7,9 +16,93 @@ package com.forrest.core.services.ingestion;
 public class InjestJob {
 
 
-    public void injestLocalFile(String file) {
+    public InjestJob() {
+
+    }
+
+    public boolean injestLocalFile(String file) throws FileNotFoundException, IOException {
+
+        injestCSV(file);
+        return true;
+
+    }
 
 
+    public void injestCSV(String file) {
+
+        LinkedList<String> fileTypes = getColumnTypesCsv(file);
+
+        LinkedList<String> columnNames = new LinkedList<String>();
+
+        for (int i = 0; i < fileTypes.size(); i++) {
+            columnNames.add("col" + i);
+        }
+
+        String query = getHiveFlatTableCreationQueryExternal(columnNames, fileTypes, file, "default","testTable","\\n",",");
+
+        System.out.println("Query:" + query);
+
+    }
+
+    public String getHiveFlatTableCreationQueryExternal(LinkedList<String> names, LinkedList<String> types, String file, String database, String tableName, String rowDelimiter, String colDelmiter) {
+
+        String query = "CREATE EXTERNAL TABLE " + tableName + "_external (";
+
+        for(int i=0; i<names.size(); i++) {
+            query += names.get(i) + " " + types.get(i) + ",";
+        }
+
+        query = query.substring(0,query.length() - 1);
+
+        String abbreviatedLocation = file.substring(0,file.lastIndexOf("/"));
+        System.out.println("Abbreviated Location:" + abbreviatedLocation);
+
+        query += ") ROW FORMAT DELIMITED FIELDS TERMINATED BY '" + colDelmiter + "' LINES TERMINATED BY '" + rowDelimiter + "' LOCATION '" + abbreviatedLocation + "'; ";
+
+        query += "CREATE TABLE " + tableName + " AS SELECT * FROM " + tableName + "_external WHERE instr(INPUT__FILE__NAME,'"+file+"')!=0; DROP TABLE " + tableName + "_external;";
+
+        return query;
+
+    }
+
+    public LinkedList<String> getColumnTypesCsv(String file) {
+
+        LinkedList<String> fileTypes = new LinkedList<String>();
+
+        try {
+
+            CSVReader reader = new CSVReader(new FileReader(file));
+            reader.readNext();
+            String[] secondLine = reader.readNext();
+
+            for (String eachCol : secondLine) {
+                fileTypes.add(getType(eachCol));
+            }
+
+            System.out.println(fileTypes);
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+
+        }
+
+        return fileTypes;
+
+    }
+
+    public String getType(String value) {
+
+        if (value.toLowerCase().equals("true") || value.toLowerCase().equals("false") || value.toLowerCase().equals("t") || value.toLowerCase().equals("f"))
+            return "BOOLEAN";
+        else if ((!value.matches(".*\\d.*")) || value.matches(".*[A-Z].*") || value.matches(".*[a-z].*"))
+            return "STRING";
+        else if (value.contains("."))
+            return "DOUBLE";
+        else
+            return "INT";
 
     }
 
--- src/test/java/com/forrest/core/services/ingestion/InjestJobTest.java
+++ src/test/java/com/forrest/core/services/ingestion/InjestJobTest.java
@@ -2,11 +2,16 @@ package com.forrest.core.services.ingestion;
 
 import au.com.bytecode.opencsv.CSVReader;
 import com.forrest.web.config.StandaloneConfig;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.io.FileReader;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.easymock.EasyMock.*;
 
@@ -15,13 +20,72 @@ import static org.easymock.EasyMock.*;
  * @date 11/30/13
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes=StandaloneConfig.class, loader=AnnotationConfigContextLoader.class)
+@ContextConfiguration(classes = StandaloneConfig.class, loader = AnnotationConfigContextLoader.class)
 public class InjestJobTest {
+
+    private InjestJob job;
+
+
+    @Before
+    public void setup() {
+        job = new InjestJob();
+
+    }
 
     @Test
     public void injestLocalFile() throws Exception {
-        CSVReader csvReader = createMock(CSVReader.class);
-        replay(csvReader);
-        verify(csvReader);
+
+        if (job.injestLocalFile("/Users/atrask/Dropbox/ForRest/src/test/resources/data/test.csv"))
+            assert true;
+        else
+            assert false;
     }
+
+    @Test
+    public void testIntDetection() {
+        boolean passed = true;
+
+        if(!job.getType("1234").equals("INT"))
+            passed = false;
+
+        assert passed;
+    }
+
+    @Test
+    public void testStringDetection() {
+
+     boolean passed = true;
+
+        if(!job.getType("hello").equals("STRING"))
+            passed = false;
+        if(!job.getType("hello1234").equals("STRING"))
+            passed = false;
+
+        assert passed;
+    }
+
+    @Test
+    public void testDoubleDetection() {
+
+        boolean passed = true;
+
+        if(!job.getType("12.34").equals("DOUBLE"))
+            passed = false;
+
+        assert passed;
+
+    }
+
+    @Test
+    public void testBooleanDetection() {
+
+        boolean passed = true;
+
+        if(!job.getType("TRUE").equals("BOOLEAN"))
+            passed = false;
+
+
+        assert passed;
+    }
+
 }
