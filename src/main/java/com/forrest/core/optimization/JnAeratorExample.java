8c10d9afa57ad6de8e6e8734c2021120433a6816
iamtrask <liamtrask@gmail.com>
2013-12-07 03:00:53 -0600

    added jnaerator test example for porting c++ code to Java... i plan on using this to port llvm to java... llvm is why impala is so fast

---
+++ src/main/java/com/forrest/core/optimization/JnAeratorExample.java
@@ -0,0 +1,12 @@
+package com.forrest.core.optimization;
+
+/**
+ * Description of file content.
+ *
+ *  mvn com.nativelibs4java:maven-jnaerator-plugin:generate
+ *
+ * @author atrask
+ *         12/7/13
+ */
+public class JnAeratorExample {
+}
---
+++ src/main/scala/com/forrest/ds/algorithms/GibbsSampling.scala
@@ -0,0 +1,16 @@
+package com.forrest.ds.algorithms
+
+/**
+ * Gibbs sampling referencing from:
+ * http://www.cs.umd.edu/~hardisty/papers/gsfu.pdf
+ *
+ * @author atrask
+ *         12/7/13
+ */
+class GibbsSampling {
+
+  def main(args: Array[String]) {
+    println("Hello, world!")
+  }
+
+}
